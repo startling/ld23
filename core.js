be259bf23b15deb4925589165ac1605112605dd2
tim <tdixon51793@gmail.com>
2012-04-20 21:34:53 -0500

    initialize the canvas and stuff.

---
+++ core.js
@@ -0,0 +1,23 @@
+/* core stuff. */
+
+// and constants for tile height and width and page height and width (in
+// number of tiles, not pixels).
+var tile_size = 20;
+var page_height = 26;
+var page_width = 25;
+
+
+// main list of stages to play.
+levels = [_base_stage]
+
+
+// stuff to do when the page finishes loading:
+window.addEventListener('load', function () {
+    // get the canvas element
+    var canvas = document.getElementsByTagName("canvas")[0];
+    // set its height and width.
+    canvas.width = tile_size * page_width;
+    canvas.height = tile_size * page_height;
+    // get its 2d context.
+    var context = canvas.getContext("2d");
+}, false);
