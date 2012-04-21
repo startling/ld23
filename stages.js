dcc66bfd28fb25dcec8eb5ebccaf7285019c1cbc
tim <tdixon51793@gmail.com>
2012-04-21 15:20:07 -0500

    add an npc to the test stage.

--- stages.js
+++ stages.js
@@ -146,4 +146,5 @@ function Stage (literal) {
 var first = Stage({
     players: [player.at(0, 2), player.at(2, 2)],
     obstacles: [block.at(1, 1), block.at(1, 2), block.at(1, 3)],
+    npcs: [npc.at(5, 5)]
 });
